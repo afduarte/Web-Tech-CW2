6ac37741409c2755ea04fdde041975726e5bf97a
Antero Duarte <a.fduarte1@gmail.com>
2017-03-24 17:27:21 +0000

    Layout for form in place, multiple editing

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,14 @@
-$(document).foundation()
+$(function(){
+  // Event Handling
+  // -- Date changes
+  $('.range.q-slider input').on('input change', function (e) {
+    $(this).next('output').text(data.answers[$(this).val() - 1])
+  });
+  // Top slider
+  $('.range.cat-slider input').on('input', function(e){
+    var selected = $(this);
+    selected.next('output').text(data.topAnswers[selected.val() - 1])
+    console.log("running top slider event: "+selected.val());
+    $(this).parent().parent().find('.range.q-slider input').val(selected.val()).change();
+  })
+})
--- routes/input.js
+++ routes/input.js
@@ -14,18 +14,23 @@ router.get('/', function (req, res, next) {
 
   connection.connect();
   // Query to get all the questions
-  // SELECT QUE_CODE, QUE_NAME, QUE_TEXT, CAT_NAME from INS_QUE JOIN INS_CAT ON INS_QUE.CAT_CODE=INS_CAT.CAT_CODE;
   return utils.query(`
-  SELECT QUE_CODE, QUE_NAME, QUE_TEXT, CAT_NAME
+  SELECT QUE_CODE, QUE_NAME, QUE_TEXT, CAT_NAME, INS_QUE.CAT_CODE
     FROM INS_QUE 
     JOIN INS_CAT ON INS_QUE.CAT_CODE=INS_CAT.CAT_CODE;`, 
   connection)
     .then(({ results, fields }) => {
-      console.log(results);
       connection.end();
+      let categories = {};
+      for(let row of results){
+        categories[row.CAT_CODE] = categories[row.CAT_CODE] || {name:row.CAT_NAME, questions:[]}
+        categories[row.CAT_CODE].questions.push({code:row.QUE_CODE,name:row.QUE_NAME,text:row.QUE_TEXT})
+      }
       return res.render('input', {
         data: {
-          questions: results
+          questions: categories,
+          answers: ["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"],
+          topAnswers: ["Very Bad", "Bad", "Neutral", "Good", "Very Good"]
         }
       });
     })
